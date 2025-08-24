1dc172b809d2741c873b8f06a22c337d029bd213
Vinay <iamvinayy100@gmail.com>
2025-08-24 22:17:01 +0530

    auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "JobFit AI" },
+    { name: "description", content: "AI-powered resume checker for better job matches." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Smart Resume Checker for Perfect Job Matches</h1>
+        <h2>Smart resume optimization made simple</h2>
+      </div>
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+      {resumes.map((resume) => (
+        <ResumeCard key={resume.id} resume={resume} />
+      ))}
+    </div>
+
+)}
+</section>
+
+  </main>;
 }
