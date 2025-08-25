984443b9e8e84f92ed019fd1f5b20c765231d3f9
Vinay <iamvinayy100@gmail.com>
2025-08-25 12:27:21 +0530

    Authentication

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router';
+import { usePuterStore } from '~/lib/puter'
 
 export const meta = () => ([
     {title: 'JobFit AI | Auth'},
@@ -6,6 +8,13 @@ export const meta = () => ([
 ])
 
 const auth = () => {
+    const {isLoading, auth} = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next])      
   return (
     <main className='bg-[url("/images/bg-main.svg")] bg-cover min-h-screen flex items-center justify-center'>
         <div className='gradient-border shadow-lg'>
@@ -13,6 +22,25 @@ const auth = () => {
                 <div className='flex flex-col items-center gap-2 text-center'>
                     <h1>Welcome</h1>
                     <h2>Log In to Continue</h2>
+                </div>
+                <div>
+                    {isLoading ? (
+                        <button className='auth-button animate-pulse'>
+                            <p>Just a moment...</p>
+                        </button>
+                    ) : (
+                        <>
+                            {auth.isAuthenticated ? (
+                                <button className='auth-button' onClick={auth.signOut}>
+                                    <p>Log Out</p>
+                                </button>
+                            ) : (
+                                <button className='auth-button' onClick={auth.signIn}>
+                                    <p>Log In</p>
+                                </button>
+                            )}
+                        </>
+                    )}
                 </div>
             </section>
         </div>
