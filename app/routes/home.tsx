dc48214064707b1654573f13fe0dcd24ff766163
Vinay <iamvinayy100@gmail.com>
2025-08-29 21:29:52 +0530

    almost finished

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,43 +1,78 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
-
+import resume from "./resume";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "JobFit AI" },
-    { name: "description", content: "AI-powered resume checker for better job matches." },
+    {
+      name: "description",
+      content: "AI-powered resume checker for better job matches.",
+    },
   ];
 }
 
 export default function Home() {
-  const {auth} = usePuterStore();
-    const navigate = useNavigate();
-    useEffect(() => {
-        if(!auth.isAuthenticated) navigate('/auth?next=/');
-    }, [auth.isAuthenticated])  
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
 
-    <section className="main-section">
-      <div className="page-heading py-16">
-        <h1>Smart Resume Checker for Perfect Job Matches</h1>
-        <h2>Smart resume optimization made simple</h2>
-      </div>
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
 
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-      {resumes.map((resume) => (
-        <ResumeCard key={resume.id} resume={resume} />
-      ))}
-    </div>
+      const resumes = (await kv.list("resume:*", true)) as KVItem[];
 
-)}
-</section>
+      const parsedResumes = resumes?.map(
+        (resume) => JSON.parse(resume.value) as Resume
+      );
+      console.log("parsedResumes", parsedResumes);
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
+    };
+    loadResumes();
+  }, []);
 
-  </main>;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Smart Resume Checker for Perfect Job Matches</h1>
+          {!loadingResumes && resumes?.length == 0 ? (
+            <h2>No Resumes found! Upload your first resume to get feedback.</h2>
+          ) : (
+            <h2>Smart resume optimization made simple</h2>
+          )}
+        </div>
+        {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+        )}
+
+        {!loadingResumes && resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+        {!loadingResumes && resumes?.length == 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to='/upload' className="primary-button w-fit text-xl font-semibold">Upload Resume</Link>
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
